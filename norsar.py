5c383f9c7f68a455e2ee75a1b69499e7e33f5118
drb <dag.robole@protonmail.com>
2025-10-04 21:58:20 +0200

    Added file management functions

--- norsar.py
+++ norsar.py
@@ -1,33 +1,97 @@
 # -*- coding: utf-8 -*-
 
 import norsar_log
-import sys, os, pyodbc
+import sys, os, subprocess, pyodbc
 from pathlib import Path
+from shutil import move, copy
+from win32file import CreateFile, CloseHandle
+from win32con import GENERIC_READ, GENERIC_WRITE, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL
 
-dir_import = Path("C:\\temp")
+DIR_IMPORT = "C:\\temp"
 
 # Exit codes for this program
 class ExitStatus: Success, Running, Error = range(3)
 
-def fetch_files(log, dirname):
-    pass
+class NorsarContext():
+    # Define a class used to hold contextual information    
+    def __init__(self, script_dir):                    
+        # Declare variables for all norsar directories, and create them if they don't exist already
+        self.directory_script = script_dir
+        self.directory_import = Path(DIR_IMPORT)
+        self.directory_work = self.directory_script / "work"        
+        self.directory_ignore = self.directory_script / "ignore"  
+        
+        os.makedirs(self.directory_work, exist_ok = True) # Create work directory
+        os.makedirs(self.directory_ignore, exist_ok = True) # Create ignore directory
+        
+def has_exclusive_access(filename):    
+    # Check if we have exclusive access to a file
+    try:
+        handle = CreateFile(str(filename), GENERIC_READ | GENERIC_WRITE, 0, None, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, None)
+        CloseHandle(handle)
+    except:
+        return False            
+    return True
 
-def import_norsar_files(dirname):    
+def fetch_files(log, ctx):    
+    ifiles = ctx.directory_import.glob("*.txt")
+    for file in ifiles:
+        try:
+            # If we don't have full access to the file, skip it for now
+            if not has_exclusive_access(file):
+                log.info("Unable to aquire exclusive access to " + str(file))
+                continue            
+                        
+            log.info("Moving file " + str(file) + " to work directory")
+            move(file, ctx.directory_work)            
+            
+        except Exception as ex:
+            log.error(str(ex), exc_info=True)
+            
+    return ctx.directory_work.glob("*.txt")
+
+def analyze_file(log, ctx, file):
+    log.info("Analyzing file " + str(file)) 
+    copy(ctx.directory_script / "c1_def.INP", ctx.directory_script / "IO_BUFF.$$$")
+    move(file, ctx.directory_script / "CTBTphd.txt")
+    log.info("Running ctbtcon.exe") 
+    subprocess.run([str(ctx.directory_script / "ctbtcon.exe")])
+    log.info("Running gamma.exe") 
+    subprocess.run([str(ctx.directory_script / "gamma.exe")])
+    log.info("Analysis done")     
+
+def ignore_file(log, ctx, file):
+    log.info("Ignoring file " + str(file))
+    move(file, ctx.directory_ignore)                
+
+def import_file(log, ctx, file):    
+    found = False
+    with file.open() as fd:    
+        lines = [line.rstrip() for line in fd]
+    for i in range(len(lines)):
+        if lines[i] == "#Header 3":
+            if lines[i+1].endswith("FULL"):
+                analyze_file(log, ctx, file)
+                found = True
+                break
+    if found == False:
+        ignore_file(log, ctx, file)            
+    
+def import_norsar_files():    
     try:        
         log = norsar_log.create_log("norsar")   
         log.info("=========== START IMPORT ===========")
         
-        dir_script = Path(__file__).parent.absolute() # Path to this script
-        dir_work = dir_script / "work" # Path to work directory
-        os.makedirs(dir_work, exist_ok = True) # Create work directory
-        
-        files = fetch_files(log, dirname)
+        ctx = NorsarContext(Path(__file__).parent.absolute())                        
+        files = fetch_files(log, ctx)
+        for file in files:
+            import_file(log, ctx, file)
         
         log.info("=========== END IMPORT ===========")
-        sys.exit(status)    
+        sys.exit(ExitStatus.Success)    
     except Exception as ex:            
         print(str(ex))    
         sys.exit(ExitStatus.Error)        
 
 if __name__ == "__main__":
-    import_norsar_files(dir_import)
+    import_norsar_files()
